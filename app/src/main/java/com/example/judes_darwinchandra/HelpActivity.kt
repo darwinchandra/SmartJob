e69ae491ea3df8e35a13732f1f3d6e2948063a20
julian fellyco <julianfellyco85@gmail.com>
2021-03-07 21:39:56 +0700

    intent

--- app/src/main/java/com/example/judes_darwinchandra/HelpActivity.kt
+++ app/src/main/java/com/example/judes_darwinchandra/HelpActivity.kt
@@ -1,13 +1,15 @@
 package com.example.judes_darwinchandra
 
 import android.os.Build
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_help.*
-import kotlinx.android.synthetic.main.activity_video_call.*
 
 class HelpActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -24,5 +26,29 @@ class HelpActivity : AppCompatActivity() {
         topAppBar_help.setNavigationOnClickListener {
             finish()
         }
+
+
+        fun composeEmail(addresses: Intent, subject: String, attachment: Uri): Intent? {
+
+            val intent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/html"
+                putExtra(Intent.EXTRA_EMAIL, "example@gmail.com")
+                putExtra(Intent.EXTRA_SUBJECT, "subject")
+                putExtra(Intent.EXTRA_TEXT, "Email message text")
+                putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"))
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(Intent.createChooser(intent, "Send Email"))
+            }
+        }
+        button3.setOnClickListener(
+            startActivity(composeEmail(intent))
+        )
     }
 }
+
+private fun Button.setOnClickListener(startActivity: Unit) {
+    TODO("Not yet implemented")
+}
+
+
