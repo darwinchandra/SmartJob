e8a18b9dd5f8c16ae2c46c68120148e9f805413d
Seteven Ang <steven.ang12377@gmail.com>
2021-03-14 16:35:40 +0700

    Thread

--- app/src/main/java/com/example/judes_darwinchandra/ManageProfilActivity.kt
+++ app/src/main/java/com/example/judes_darwinchandra/ManageProfilActivity.kt
@@ -1,5 +1,8 @@
 package com.example.judes_darwinchandra
 
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +10,8 @@ import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
@@ -14,7 +19,12 @@ import kotlinx.android.synthetic.main.activity_help.*
 import kotlinx.android.synthetic.main.activity_location.*
 import kotlinx.android.synthetic.main.activity_manage_profil.*
 import kotlinx.android.synthetic.main.my_custom_dialog.*
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
+//inisialisasi konstanta untuk menampung data yang akan di tampilkan saat di rotate
 private const val EXTRA_STATUS = "EXTRA_STATUS"
 class ManageProfilActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -30,22 +40,31 @@ class ManageProfilActivity : AppCompatActivity() {
         topAppBar_ManageProfil.setNavigationOnClickListener {
             finish()
         }
+        val viewimg = findViewById<ImageView>(R.id.imageProfile)
+        Thread(Runnable{
+            val bitmap = processBitMap(R.drawable.wanita)
+            viewimg.post{
+                println("Menambahkan Gambar")
+                viewimg.setImageBitmap(bitmap)
+            }
+        }).start()
     }
+    //mengoveride onsaveinstancestate untuk mengambil text pada textview untuk disimpan pada konstanta
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(EXTRA_STATUS,textViewnama.text.toString())
     }
+    //mengoveride onrestoreinstancestate untuk menampilkan text yang disimpan pada konstanta untuk ditampilkan kembali di textview
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         textViewnama.text = savedInstanceState?.getString(EXTRA_STATUS) ?: "Kosong"
     }
-
-
+//membuat function onclick getcustomdialog agar bisa menampilkan custom dialog saat tombol pencil disebelah nama ditekan
     fun getCustomDialog(view: View) {
         var Mylayout = layoutInflater.inflate(R.layout.my_custom_dialog,null)
         val mydialogbuilder : AlertDialog.Builder = AlertDialog.Builder(this).apply {
             setView(Mylayout)
-            setTitle("Ganti Nama")
+            setTitle("Masukkan")
         }
         var mydialog = mydialogbuilder.create()
         var nama = Mylayout.findViewById<EditText>(R.id.nama)
@@ -55,5 +74,15 @@ class ManageProfilActivity : AppCompatActivity() {
             mydialog.cancel()
         }
         mydialog.show()
+    }
+    private fun processBitMap(url: Int): Bitmap? {
+        return try{
+            var mybitmap = BitmapFactory.decodeResource(resources,url)
+            //proses bitmap yang lama
+            mybitmap
+        } catch (e: IOException){
+            e.printStackTrace()
+            null
+        }
     }
 }
