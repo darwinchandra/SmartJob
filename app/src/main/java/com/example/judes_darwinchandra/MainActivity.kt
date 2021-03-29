6952a051838e2d1b5e0c1762630f1ec4b7c26cd5
Seteven Ang <steven.ang12377@gmail.com>
2021-03-29 12:53:39 +0700

    Notif

--- app/src/main/java/com/example/judes_darwinchandra/MainActivity.kt
+++ app/src/main/java/com/example/judes_darwinchandra/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.judes_darwinchandra
 
+import android.app.NotificationChannel
+import android.app.NotificationChannelGroup
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.RingtoneManager
 import android.os.Build
 import android.os.Bundle
 import android.text.Editable
@@ -16,6 +23,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    var dataSpinner1 = arrayOf("Filter Spesialis","Ubah Nama","Chat Backup")
+    var dataSpinner2 = arrayOf("Change Password","Forgot Password")
+    var dataSpinner3 = arrayOf("Message Notification")
+    var dataSpinner4 = arrayOf("Get Recomendation")
+    var notifier_channel1 = 1
+    var notifier_channel2 = 2
+    var notificationManager : NotificationManager? = null
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,6 +93,10 @@ class MainActivity : AppCompatActivity() {
 
 
         // buat notif disini
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+        createNotificationGroups()
+        createNotificationChannels()
 
 
 
@@ -94,6 +112,93 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+    private fun createNotificationGroups() {
+//memastikan android adalah android Oreo dan mendaftarkan
+//2 grup berbeda untuk notification
+// jika ada grup berbeda, anda dapat menambahkan list
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val list = mutableListOf<NotificationChannelGroup>()
+            list.add(
+                NotificationChannelGroup("Umum",
+                    "Umum")
+            )
+            list.add(
+                NotificationChannelGroup("Email",
+                    "Email")
+            )
+            list.add(
+                NotificationChannelGroup("Chat",
+                    "Chat")
+            )
+            list.add(
+                NotificationChannelGroup("Promosi",
+                    "Promosi")
+            )
+            notificationManager!!.createNotificationChannelGroups(list)
+        }
+    }
+    private fun createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationSound = RingtoneManager
+                .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val att = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .build()
+            for (s in dataSpinner1) {
+                val Channel1 = NotificationChannel(s + "_" + "Umum", s, NotificationManager.IMPORTANCE_HIGH)
+                Channel1.setLightColor(Color.RED);
+                Channel1.enableLights(true)
+                Channel1.enableVibration(true)
+                Channel1.group = "Umum"
+                Channel1.vibrationPattern =
+                    longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 100)
+                Channel1.setSound(notificationSound, att);
+
+                if(notificationManager!=null){
+                    notificationManager?.createNotificationChannel(Channel1)
+                }
+            }
+            for(s in dataSpinner2){
+                val Channel2 = NotificationChannel(s + "_" + "Email", s, NotificationManager.IMPORTANCE_NONE)
+                Channel2.setLightColor(Color.RED);
+                Channel2.enableLights(true)
+                Channel2.enableVibration(true)
+                Channel2.group = "Email"
+                Channel2.vibrationPattern =
+                    longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 100)
+                Channel2.setSound(notificationSound, att);
+                if(notificationManager!=null){
+                    notificationManager?.createNotificationChannel(Channel2)
+                }
+            }
+            for(s in dataSpinner3){
+                val Channel3 = NotificationChannel(s + "_" + "Chat", s, NotificationManager.IMPORTANCE_NONE)
+                Channel3.setLightColor(Color.RED);
+                Channel3.enableLights(true)
+                Channel3.enableVibration(true)
+                Channel3.group = "Chat"
+                Channel3.vibrationPattern =
+                    longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 100)
+                Channel3.setSound(notificationSound, att);
+                if(notificationManager!=null){
+                    notificationManager?.createNotificationChannel(Channel3)
+                }
+            }
+            for(s in dataSpinner4){
+                val Channel4 = NotificationChannel(s + "_" + "Promosi", s, NotificationManager.IMPORTANCE_NONE)
+                Channel4.setLightColor(Color.RED);
+                Channel4.enableLights(true)
+                Channel4.enableVibration(true)
+                Channel4.group = "Promosi"
+                Channel4.vibrationPattern =
+                    longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 100)
+                Channel4.setSound(notificationSound, att);
+                if(notificationManager!=null){
+                    notificationManager?.createNotificationChannel(Channel4)
+                }
+            }
+        }
     }
     fun cekvalid(validasi:Array<Int>){
 
