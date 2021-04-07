20f6a456eacf64e75184d3a22c6f59e4ac64dbc0
Seteven Ang <steven.ang12377@gmail.com>
2021-04-07 23:22:50 +0700

    comment

--- app/src/main/java/com/example/judes_darwinchandra/News.kt
+++ app/src/main/java/com/example/judes_darwinchandra/News.kt
@@ -18,29 +18,43 @@ import org.json.JSONObject
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class News: JobService() {
+    //Menympan apikey,country, dan kategori yang didapat dalam variabel AppID
     val AppID = "81c1172db5154c208b2e21d76b4dab56"
     val country = "id"
     val kategori = "business"
-
+    //onStartJob akan dijalankan ketika scheduler telah aktif sesuai jadwal yang ditentukan
     override fun onStartJob(params: JobParameters?): Boolean {
+        //menampilkan log bahwa job telah dimulai
         Log.w("Job","Mulai")
+        //menjalankan fungsi get News untuk mendapatkan hasil
         getNews(params)
+        //mengembalikan sebuah nilai Boolean, true menyatakan proses akan berjalan di thread yang berbeda
         return true
     }
+    //onStopJo akan dijalankan ketika scheduler anda sedang berjalan tetapi berhenti ketika kondisinya tidak dipenuhi
     override fun onStopJob(params: JobParameters?): Boolean {
         Log.w("Job","Berhenti")
+        //Mengembalikan True, ketika service scheduler yang anda jalankan, gagal karena suatu proses sehingga anda perlu menjadwalkan kembali proses yang akan dilakukan
         return true
     }
 
     private fun getNews(JobParameters: JobParameters?) {
+        //var client digunakan untuk membuat objek AsyncHttpClinet(), yang akan memproses penerimaan data dari Internet secara Async melalui AsyncHttpResponseHandler()
         var client = AsyncHttpClient()
+        //Var url, merupakan url khusus yang akan ada gunakan ketika meminta data dari sebuah web yang memberikan services data
         var url = "https://newsapi.org/v2/top-headlines?country=$country&category=$kategori&apiKey=$AppID"
+        //Val charset, digunakan untuk mengubah ArrayByte menjadi string melalui toString()
         val charset = Charsets.UTF_8
+        //List untuk menampung data dari news
         val list=ArrayList<MyNewsData>()
         var handler = object : AsyncHttpResponseHandler(){
+            //onSuccess() dijalankan ketika data dari web service berhasil diambil
             override fun onSuccess(
+                //berisikan kode yang dikirmkan dari web service
                 statusCode: Int,
+                //berisikan list aksi pada HTTP request dan response yang biasana bernilai kosong atau beberapa pasangan Key value
                 headers: Array<out Header>?,
+                //berisikan data yang dikirimkan dari web service
                 responseBody: ByteArray?
             ) {
                 var result = responseBody?.toString(charset) ?: "Kosong"
@@ -75,19 +89,28 @@ class News: JobService() {
                 //untuk mengirimkan extra berupa arraylist<MyNewsData> yang sudah ada datanya ke activity lain menggunakan broadcast
                 intent.putExtra(EXTRA_NEWS,list)
                 sendBroadcast(intent)
-
+                ////Ketika perintah jobFinished() dijalankan untuk memastikan job scheduler telah selesai atau belum
+                //false menandakan bahwa job  tidak bermasalah dan job selesai
                 jobFinished(JobParameters,false)
             }
-
+            //onFailure() dijalankan ketika data dari web service gagal diambil
             override fun onFailure(
+                //berisikan kode yang dikirmkan dari web service
                 statusCode: Int,
+                //berisikan list aksi pada HTTP request dan response yang biasana bernilai kosong atau beberapa pasangan Key value
                 headers: Array<out Header>?,
+                //berisikan data yang dikirimkan dari web service
                 responseBody: ByteArray?,
+                //berisikan Exeption yang mengakibatkan kegagalan permintaan data
                 error: Throwable?
             ) {
+                //Ketika perintah jobFinished() dijalankan untuk memastikan job scheduler telah selesai atau belum
+                //true menandakan bahwa jika job bermasalah maka akan dijalankan ulang
                 jobFinished(JobParameters,true)
             }
         }
+        //Untuk mengambil data menggunakan AsyncHttpClinet(),menggunakan fungsi get() yang memerlukan 2 parameter, yaitu alamat pengambilan data (url) dan
+        //objek AsyncHttpResponseHandler() yang menangani data ketika diterima respon dari web service
         client.get(url,handler)
     }
 }
