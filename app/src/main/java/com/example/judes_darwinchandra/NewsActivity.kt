2bc31c2d9d97cf24db5ba5991926c72194e1561c
darwinchandra <darwin.chandra0@gmail.com>
2021-04-07 21:21:55 +0700

    tampil news2

--- app/src/main/java/com/example/judes_darwinchandra/NewsActivity.kt
+++ app/src/main/java/com/example/judes_darwinchandra/NewsActivity.kt
@@ -68,9 +68,7 @@ class NewsActivity : AppCompatActivity() {
             .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
             .setRequiresDeviceIdle(false)
             .setRequiresCharging(false)
-            .setPeriodic(3*60*1000)
         var JobNews = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
         JobNews.schedule(mJobInfo.build())
-        Toast.makeText(this,"Job Service Berjalan", Toast.LENGTH_SHORT).show()
     }
 }
