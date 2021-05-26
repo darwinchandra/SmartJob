31c5dc33635b831b0130b15ac58943a955bf250f
Seteven Ang <steven.ang12377@gmail.com>
2021-05-26 22:24:01 +0700

    Comment

--- app/src/main/java/com/example/judes_darwinchandra/MainActivity.kt
+++ app/src/main/java/com/example/judes_darwinchandra/MainActivity.kt
@@ -75,8 +75,6 @@ class MainActivity : AppCompatActivity() {
         ).build()
 
         login_button.setOnClickListener{
-            //hasil untuk menampung data yang akan diinput
-            var hasil =""
             var findData = false
             //berisi email dan password yang diinput dan di konversi kedalam bentuk string
             var mailLogin = inputEmail.text.toString()
@@ -101,8 +99,6 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(it,"Username dan Password Tidak Cocok" , Toast.LENGTH_SHORT).show()
                         inputEmail.requestFocus()
                     }
-                    //menampilkan log berupa data pada database yang telah kita simpan pada variabel hasil
-                    Log.w("tes",hasil)
                 }
 
             }
